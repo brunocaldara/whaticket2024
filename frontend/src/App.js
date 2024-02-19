5d9f4f0afdcaa8c6f796cae8c9e41e00ad09d189
zaltvapp <45437371+zaltvapp@users.noreply.github.com>
2024-02-18 22:21:44 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -37,7 +37,7 @@ const App = () => {
                 },
                 "&::-webkit-scrollbar-thumb": {
                     boxShadow: 'inset 0 0 6px rgba(0, 0, 0, 0.3)',
-                    backgroundColor: "#2DDD7F",
+                    backgroundColor: "#342f6f",
                 },
             },
             scrollbarStylesSoft: {
@@ -50,7 +50,7 @@ const App = () => {
             },
             palette: {
                 type: mode,
-                primary: { main: mode === "light" ? "#2DDD7F" : "#FFFFFF" },
+                primary: { main: mode === "light" ? "#342f6f" : "#FFFFFF" },
                 textPrimary: mode === "light" ? "#2DDD7F" : "#FFFFFF",
                 borderPrimary: mode === "light" ? "#2DDD7F" : "#FFFFFF",
                 dark: { main: mode === "light" ? "#333333" : "#F3F3F3" },
@@ -73,7 +73,7 @@ const App = () => {
                 total: mode === "light" ? "#fff" : "#222",
                 messageIcons: mode === "light" ? "grey" : "#F3F3F3",
                 inputBackground: mode === "light" ? "#FFFFFF" : "#333",
-                barraSuperior: mode === "light" ? "linear-gradient(to right, #2DDD7F, #2DDD7F , #2DDD7F)" : "#666",
+                barraSuperior: mode === "light" ? "linear-gradient(to right, #342f6f, #3d2f88 , #6f7dec)" : "#666",
 				boxticket: mode === "light" ? "#EEE" : "#666",
 				campaigntab: mode === "light" ? "#ededed" : "#666",
 				mediainput: mode === "light" ? "#ededed" : "#1c1c1c",
